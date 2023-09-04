546335f06c74097997da91928a0a5d9f54a4a4cf
ng3rdstmadgke <ng3rdstmadgke@gmail.com>
2023-09-04 11:40:37 +0900

    テストAPI

--- sample-app/main.py
+++ sample-app/main.py
@@ -23,6 +23,67 @@ def world():
     env = get_env()
     return {"world": env.api_gateway_base_path}
 
+from fastapi import FastAPI, Request, Response
+import json
+
+# Starlette - Request: https://www.starlette.io/requests/
+# Starlette - Response: https://www.starlette.io/responses/
+@app.get("/api/get_test")
+async def get_test(request: Request, ):
+    print(request.headers)
+    print(request.url)
+    print(request.method)
+    print(request.query_params)
+    print(request.client)
+    print(await request.body())
+    response = {"url": str(request.url)}
+    return Response(
+        content=json.dumps(response),
+        status_code=200,
+        headers={"hoge": "fuga"},
+        media_type="application/json",
+    )
+
+from pydantic import BaseModel
+
+class PostSchema(BaseModel):
+    name: str
+    age: int
+
+@app.post("/api/post_test")
+async def post_test(
+    data: PostSchema,
+    request: Request
+):
+    print(request.headers)
+    print(request.url)
+    print(request.method)
+    print(request.query_params)
+    print(request.client)
+    print(await request.json())
+    response = {"url": str(request.url)}
+    return Response(
+        content=json.dumps(response),
+        status_code=200,
+        headers={"hoge": "fuga"},
+        media_type="application/json",
+    )
+
+import boto3
+@app.get("/api/list_bucket")
+def list_bucket(request: Request):
+    client = boto3.client('s3')
+    response = client.list_buckets()
+    buckets = [bucket['Name'] for bucket in response['Buckets']]
+    print(response)
+    return Response(
+        content=json.dumps(buckets),
+        status_code=200,
+        headers=None,
+        media_type="application/json",
+    )
+
+
 app.mount("/", StaticFiles(directory=f"./static", html=True), name="static")
 
 handler = Mangum(app)
